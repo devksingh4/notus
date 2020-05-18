c1bb0998814398bfe456eae26a907ebd635e3ef5
Dev Singh <dev@singhk.dev>
2020-05-18 16:22:55 -0500

    get some basic shit in here
    Signed-off-by: Dev Singh <dev@singhk.dev>

--- src/Designer.js
+++ src/Designer.js
@@ -1,6 +1,37 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
+import MyCatalog from './catalog/mycatalog';
+import {Map} from 'immutable';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+import {
+  Models as PlannerModels,
+  reducer as PlannerReducer,
+  ReactPlanner,
+  Plugins as PlannerPlugins,
+} from 'react-planner';
+
+//define state
+let AppState = Map({
+  'react-planner': new PlannerModels.State()
+});
+ 
+//define reducer
+let reducer = (state, action) => {
+  state = state || AppState;
+  state = state.update('react-planner', plannerState => PlannerReducer(plannerState, action));
+  return state;
+};
+ 
+let store = createStore(reducer, null, window.devToolsExtension ? window.devToolsExtension() : f => f);
+ 
+let plugins = [
+  PlannerPlugins.Keyboard(),
+  PlannerPlugins.Autosave('react-planner_v0'),
+  PlannerPlugins.ConsoleDebugger(),
+];
 function Designer() {
   return (
     <div className="Designer">
@@ -10,6 +41,17 @@ function Designer() {
           <p></p>
         </Container>
       </Jumbotron>
+      <Provider store={store}>
+      <Container>
+      <ReactPlanner
+        catalog={MyCatalog}
+        width={1000}
+        height={1000}
+        plugins={plugins}
+        stateExtractor={state => state.get('react-planner')}
+      />
+      </Container>
+    </Provider>
     </div>
   );
 }
---
+++ src/catalog/mycatalog.js
@@ -0,0 +1,15 @@
+import {Catalog} from 'react-planner';
+
+import * as Areas from './areas/area/planner-element.jsx';
+import * as Lines from './lines/wall/planner-element.jsx';
+import * as Door from './holes/door/planner-element.jsx';
+import * as DoorDouble from './holes/door-double/planner-element.jsx';
+const Wind = import('./holes/window/planner-element.jsx');
+let catalog = new Catalog();
+for( let x in Areas ) catalog.registerElement( Areas[x] );
+for( let x in Lines ) catalog.registerElement( Lines[x] );
+for( let x in Door ) catalog.registerElement( Door[x] );
+for( let x in DoorDouble ) catalog.registerElement( DoorDouble[x] );
+for( let x in Wind ) { catalog.registerElement( Wind[x] ); };
+
+export default catalog;
