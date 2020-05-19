7ef9c830fe86e0ca389c8efb96c48b9d044ee432
Arthur Lu <learthurgo@gmail.com>
2020-05-18 19:27:32 -0500

    getting started
    Signed-off-by: Arthur Lu <learthurgo@gmail.com>

---
+++ src/model.js
@@ -0,0 +1,17 @@
+class Person {
+    constructor(x, y, inf, targetx, targety){
+        
+    }
+    get x(){
+        return this.x
+    }
+    get y(){
+        return this.y
+    }
+    set x(nx){
+        self.x = nx
+    }
+    set y(ny){
+        self.y = ny
+    }
+}
