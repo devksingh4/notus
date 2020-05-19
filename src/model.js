7f142e7e27d9d01fd40d918e62c7faf32ccb0061
Arthur Lu <learthurgo@gmail.com>
2020-05-18 19:39:30 -0500

    made setters and getters
    Signed-off-by: Arthur Lu <learthurgo@gmail.com>

--- src/model.js
+++ src/model.js
@@ -1,17 +1,45 @@
 class Person {
-    constructor(x, y, inf, targetx, targety){
-        
+    constructor(nx, ny, ninf, ntargetx, ntargety, nkaren){ // coords are in meters, the karen modifier indicates the level of stubborness to anti-plague measures
+        this.x = nx;
+        this.y = ny;
+        this.targetx = ntargetx;
+        this.targety = ntargety;
+        this.karen = nkaren;
     }
     get x(){
-        return this.x
+        return this.x;
     }
     get y(){
-        return this.y
+        return this.y;
+    }
+    get inf(){
+        return this.inf;
+    }
+    get targetx(){
+        return this.targetx;
+    }
+    get targety(){
+        return this.targety;
+    }
+    get karen(){
+        return this.karen;
     }
     set x(nx){
-        self.x = nx
+        self.x = nx;
     }
     set y(ny){
-        self.y = ny
+        self.y = ny;
+    }
+    get inf(ninf){
+        this.inf = ninf;
+    }
+    get targetx(ntargetx){
+        this.targetx = ntargetx;
+    }
+    get targety(ntargety){
+        this.targety = ntargety;
+    }
+    get karen(nkaren){
+        this.karen = nkaren;
     }
 }
