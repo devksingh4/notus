05080f034d60f162880d42e8959315eca58abd01
Arthur Lu <learthurgo@gmail.com>
2020-05-21 18:42:08 -0500

    added circle-square intersection finder
    Signed-off-by: Arthur Lu <learthurgo@gmail.com>

--- src/model.js
+++ src/model.js
@@ -8,3 +8,36 @@ module.exports.process = async (data) => {
     // console.log(p.toString());
     return true;
 };
+
+function sA(r, x, y){
+    var a;
+    if(x < 0){
+        return -sA(r, -x, y);
+    }
+    if (y < 0) {
+		return -sA(r, x, -y);
+	}
+	if (x > r) {
+		x = r;
+	}
+
+	if (y > r) {
+		y = r;
+    
+    if (x*x + y*y > r*r) {
+		a = r*r*asin(x/r) + x*sqrt(r*r-x*x) + r*r*asin(y/r) + y*sqrt(r*r-y*y) - r*r*M_PI_2;
+		a *= 0.5;
+	} else {
+		a = x*y;
+	}
+	return a;
+}
+/*
+Given P_rect_top_left(x1, y1), P_rect_bottom_right(x2, y2), 
+P_circle_center(mx, my) and radius(r), calc(x1, y1, x2, y2, mx, my, r) 
+returns the area of intersection.
+*/
+function calc(){x1, y1, x2, y2, mx, my, r}{ 
+    x1-=mx; x2-=mx; y1-=my; y2-=my;
+	return sA(r, x2, y1) - sA(r, x1, y1) - sA(r, x2, y2) + sA(r, x1, y2);
+}
