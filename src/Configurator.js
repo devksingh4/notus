69a0d25995ade48eb0b56f0261d26d19ee579419
Dev <dev@singhk.dev>
2020-05-23 13:27:01 -0500

    start work on the configurator
    Signed-off-by: Dev <dev@singhk.dev>

--- src/Configurator.js
+++ src/Configurator.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.css';
+const electron = window.require('electron');
+const ipcRenderer  = electron.ipcRenderer;
 
-function Configurator() {
-  return (
-    <div className="Configurator">
+export default class Configurator extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {} // default values and original values
+    // this.handleChange1 = this.handleChange1.bind(this); // bind change listener to the element which runs that on change
+  }
+  componentDidMount = () => {
+    // this.setState({}) default values for the form. 
+    // get the current values from config.json
+  }
+  render() {
+    return (
+      <div className="Configurator">
       <Jumbotron fluid>
         <Container>
           <h1>Room Configurator</h1>
           <p></p>
         </Container>
       </Jumbotron>
+      <Container>
+        <Form>
+        <Form.Group controlId="">
+          <Form.Label>Item 1</Form.Label>
+          <Form.Control type="number" min="0" max="100" value={this.state.item1} onChange={this.handleChange1} placeholder={this.state.orig1} />
+        </Form.Group>
+        <Button variant="primary" onClick={this.applyChanges}>
+          Submit
+        </Button>        
+        <Button variant="danger" onClick={this.setDefaults}>
+          Reset to Defaults
+        </Button>
+      </Form>
+    </Container>
     </div>
   );
+  }
 }
-
-export default Configurator;
