cd39f564ad6dd85b476034f6bc3ade6ad1dba8b5
Dev <dev@singhk.dev>
2020-05-23 19:02:34 -0500

    fix colors
    Signed-off-by: Dev <dev@singhk.dev>

--- src/Home.js
+++ src/Home.js
@@ -5,10 +5,10 @@ import Container from 'react-bootstrap/Container'
 function Home() {
   return (
     <div className="Home">
-      <Jumbotron fluid>
+      <Jumbotron fluid style={{backgroundColor: "#005faf"}}>
         <Container>
-          <h1>Notus</h1>
-          <p>Modeling the spread of COVID-19 in various room layouts</p>
+          <h1 style={{color: '#ffffff'}}>Notus</h1>
+          <p style={{color: '#ffffff'}}>Modeling the spread of COVID-19 in various room layouts</p>
         </Container>
       </Jumbotron>
       <Bricks props={[{name: "Designer"}, {name: "Configurator"}]}/>
