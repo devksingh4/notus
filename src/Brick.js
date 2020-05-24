38b9efa50504e892b9839ecccda81cd9c8ba1556
Dev <dev@singhk.dev>
2020-05-24 10:58:26 -0500

    fix react key rendering error
    Signed-off-by: Dev <dev@singhk.dev>

--- src/Brick.js
+++ src/Brick.js
@@ -12,11 +12,11 @@ export class Bricks extends Component {
     createGroup = () => {
       let group = []
       for (let i = 0; i < this.props.props.length; i++) {
-        group.push(<Link to={this.props.props[i].name.toLowerCase()}>
-          <Card style={{margin: 20}}>
+        group.push(<Link key={`${this.props.props[i].name.toLowerCase()}.Link`} to={this.props.props[i].name.toLowerCase()}>
+          <Card key={`${this.props.props[i].name.toLowerCase()}`} style={{margin: 20}}>
             <Card.Body>
-              <Card.Title style={{color: '#005faf'}}>{this.props.props[i].name}</Card.Title>
-              <Card.Text style={{color: '#005faf'}}>Enter the room {this.props.props[i].name.toLowerCase()}.</Card.Text>
+              <Card.Title key={this.props.props[i].name.toLowerCase().Title} style={{color: '#005faf'}}>{this.props.props[i].name}</Card.Title>
+              <Card.Text key={this.props.props[i].name.toLowerCase().Text} style={{color: '#005faf'}}>Enter the room {this.props.props[i].name.toLowerCase()}.</Card.Text>
             </Card.Body>
           </Card>
         </Link>)
@@ -25,7 +25,7 @@ export class Bricks extends Component {
     }
     render() {
       return (
-        <div aria-label="Property chooser">
+        <div aria-label="Property chooser" key={'opts'}>
           {this.createGroup()}
         </div>
       );
