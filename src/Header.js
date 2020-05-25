8a57fc06c21507e386392dd6669e923aad335737
Dev <dev@singhk.dev>
2020-05-24 23:09:13 -0500

    sticky header bar
    Signed-off-by: Dev <dev@singhk.dev>

--- src/Header.js
+++ src/Header.js
@@ -7,11 +7,11 @@ import { MdArrowBack } from 'react-icons/md';
 export class ScreenHeader extends Component {
     render() {
       return (
-        <div style={{display: 'flex', flex: 1, height: '6vh', padding: '1vh', fontSize: '2vh', border: 0, margin: 0, backgroundColor: "#005faf", color: "#ffffff", flexDirection: 'row'}}>
-            <div style={{marginRight: 5}}><Link to="/"><MdArrowBack style={{color: '#ffffff'}}/></Link></div>
-            <Link to="/"><div><img src={notuswhite} style={{ height: '2.5vh', marginLeft: '0.5vw', marginRight: '1vw' }}></img></div></Link>
-            <p style={{fontSize: '2.5vh'}}>{this.props.name}</p>
-        </div>
+          <div style={{display: 'flex', flex: 1, height: '6vh', padding: '1vh', fontSize: '2vh', border: 0, margin: 0, backgroundColor: "#005faf", color: "#ffffff", flexDirection: 'row', position: 'sticky', top: 0, zIndex: 1000}}>
+              <div style={{marginRight: 5}}><Link to="/"><MdArrowBack style={{color: '#ffffff'}}/></Link></div>
+              <Link to="/"><div><img src={notuswhite} alt="Notus Logo" style={{ height: '2.5vh', marginLeft: '0.5vw', marginRight: '1vw' }}></img></div></Link>
+              <p style={{fontSize: '2.5vh'}}>{this.props.name}</p>
+          </div>
       );
     }
   }
