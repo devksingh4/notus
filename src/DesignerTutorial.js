568032dba87c0591bb19725017d7f3cb528aea84
Greeky <lu_haoqin@outlook.com>
2020-05-24 20:48:47 -0500

    card layout

--- src/DesignerTutorial.js
+++ src/DesignerTutorial.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.css';
 import { ScreenHeader } from './Header';
+import notushead from './notus-doozy.png'
 
 export default class DesignerTutorial extends Component {
   render() {
@@ -14,9 +15,14 @@ export default class DesignerTutorial extends Component {
       <Card bg='light'>
         <Card.Header as="h5">Getting Started</Card.Header>
         <Card.Body>
+          <Card.Img src={notushead} style={{maxHeight: 200, margin: 20, objectFit: 'contain'}}></Card.Img>
           <Card.Title>What is Notus?</Card.Title>
           <Card.Text>
-            <i>Notus is </i>
+            <i>Notus is a tools that helps rate office layouts on the spread of COVID-19.</i>
+            <p>
+              These office layouts are defined by you, the user. Notus uses intuitive room-planning
+              tools and a powerful Monte Carlo simulator to give you relevant results.
+            </p>
           </Card.Text>
         </Card.Body>
       </Card>
@@ -26,11 +32,6 @@ export default class DesignerTutorial extends Component {
         <Card.Body>
           <Card.Title>Basic Configuration</Card.Title>
           <Card.Text></Card.Text>
-        </Card.Body>
-      </Card>
-      <br/>
-      <Card bg='light'>
-        <Card.Body>
           <Card.Title>Advanced Configuration</Card.Title>
           <Card.Text></Card.Text>
         </Card.Body>
@@ -39,6 +40,7 @@ export default class DesignerTutorial extends Component {
       <Card bg='light'>
         <Card.Header as="h5">Using the designer</Card.Header>
         <Card.Body>
+          
           <Card.Title>Drawing your office</Card.Title>
           <Card.Text></Card.Text>
         </Card.Body>
