4f8a631974bc718accedbb5be931fd2d38ffb0c2
Jacob Levine <jacoblevine18@gmail.com>
2020-05-25 01:32:16 -0500

    stupid ai bc others didn't implement it
    will be improved after the deadline. next time, let's actually
    talk about schedules first and get this shit implemented before
    1am the night of lol
    
    Signed-off-by: Jacob Levine <jacoblevine18@gmail.com>

--- src/Person.js
+++ src/Person.js
@@ -1,7 +1,7 @@
 const AirGrid = require('./AirGrid.js').AirGrid;
 const glMatrix = require('gl-matrix')
 class Person {
-    constructor(nx, ny, ninf, ntargetx, ntargety, nnon_compliant, size, airgrid, naviGrid){ // coords are in meters, the non_compliant modifier indicates the level of stubborness to anti-plague measures
+    constructor(nx, ny, ninf, ntargetx, ntargety, nnon_compliant, size, airgrid, naviGrid, infp){ // coords are in meters, the non_compliant modifier indicates the level of stubborness to anti-plague measures
         this.x = nx;
         this.y = ny;
         this.inf = ninf;
@@ -15,21 +15,22 @@ class Person {
         this.maxspeed = 1
         this.directions.pop();
         this.current_hold = null;
-
+        this.move_at = 0;
+        this.inf_prob = infp
     }
     infect(p_aerosol, d_aerosol){ // p is probability constant for infection, d is density
         var check = Math.random();
         let spread_const = this.non_compliant ? .3 : .1;
         var chance = (1-p_aerosol)**(this.size * d_aerosol* spread_const)
-        if(check > chance && this.inf === false){
-            this.inf = 0
+        if(check > chance && this.inf === 0){
+            this.inf = .01
         }
     }
     tick(time, delta_time){ // function called every time increment
         const cough_limit = this.generate_aerosols(1/10, delta_time);
         const roll = Math.random();
         if(roll < cough_limit){
-          //non-compliants don't wear masks. TODO: allow user to change this
+          //non-compliants don't wear masks. TODO: allow user to change these #s
           let directionFacing = glMatrix.vec2.create();
           let cpos = glMatrix.vec2.fromValues(this.x, this.y);
           let tpos = glMatrix.vec2.fromValues(this.targetx, this.targety);
@@ -49,7 +50,7 @@ class Person {
         let cp;
         let step = glMatrix.vec2.create();
         if(this.isWithinErrorOfTarget(.01)){
-          if (this.directions != []){
+          if (this.directions !== []){
             const newCoords = this.directions.pop();
             newVec = glMatrix.vec2.fromValues(newCoords[0], newCoords[1]);
             cp = glMatrix.vec2.fromValues(this.x, this.y);
@@ -65,6 +66,36 @@ class Person {
           newVec = glMatrix.vec2.fromValues(this.targetx, this.targety);
           cp = glMatrix.vec2.fromValues(this.x, this.y);
           glMatrix.vec2.sub(step, newVec, cp);
+          if(time > this.move_at){
+            this.move_at = Math.floor(Math.random() * (28800-time))+time;
+            if (this.current_hold == null){
+              let desk= this.naviGrid.reserveDesk();
+              if (desk != null){
+                this.current_hold = [desk, "desk"]
+                let pf = this.naviGrid(this.x, this.y, desk[0], desk[1])
+                pf.pop();
+                this.directions = pf;
+              }
+            }else if (this.current_hold[1] == "desk"){
+              this.naviGrid.unReserveDesk(this.current_hold[0]);
+              let table= this.naviGrid.reserveTable();
+              if (table != null){
+                this.current_hold = [[table[0], table[1]], "table", table[2]]
+                let pf = this.naviGrid(this.x, this.y, table[0], table[1])
+                pf.pop();
+                this.directions = pf;
+              }
+            }else {
+              this.naviGrid.unReserveTablePos(this.current_hold[0], this.current_hold[2]);
+              let desk= this.naviGrid.reserveDesk();
+              if (desk != null){
+                this.current_hold = [desk, "desk"]
+                let pf = this.naviGrid(this.x, this.y, desk[0], desk[1])
+                pf.pop();
+                this.directions = pf;
+              }
+            }
+          }
         }
         if (glMatrix.vec2.sqrLen(step) < this.maxspeed*this.maxspeed*delta_time) {
           this.x = this.targetx;
@@ -77,6 +108,8 @@ class Person {
           this.x = newpos[0];
           this.y = newpos[1];
         }
+        const dens = this.airGrid.getSquareFromCoords(this.x,this.y).getNCoronaParticles();
+        this.infect(this.inf_prob, dens);
     }
 
     isWithinErrorOfTarget(eps){
@@ -169,10 +202,10 @@ class Person {
 }
 
 class Population {
-    constructor(pop_size, starting_pos, starting_tar, starting_non_compliant, starting_size, airGrid, naviGrid){
+    constructor(pop_size, starting_pos, starting_tar, starting_non_compliant, starting_size, airGrid, naviGrid, infp){
         this.pop = [];
         for(let i = 0; i < pop_size; i++){
-            this.pop.push(new Person(starting_pos[i][0], starting_pos[i][1], starting_tar[i][0], starting_tar[i][1], starting_non_compliant[i], starting_size[i], airGrid, naviGrid));
+            this.pop.push(new Person(starting_pos[i][0], starting_pos[i][1], starting_tar[i][0], starting_tar[i][1], starting_non_compliant[i], starting_size[i], airGrid, naviGrid, infp));
         }
     }
     get size(){
